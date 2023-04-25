c8ead1ba840b02287cdca013338008b5d7f75412
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-04-25 18:31:06 +0300

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
+// Task 1
+import user from "../user.json";
+import { Profile } from "./Profile/Profile";
+// // Task 2
+import data from '../data.json';
+import { Statistics } from "./Statistics/Statistics";
+// // Task 3
+import friends from '../friends.json'
+import { FriendList } from "./FriendList/FriendList";
+// Task 4
+import transactions from '../transactions.json'
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -10,7 +23,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      
+       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
-  );
+   
+  )
 };
